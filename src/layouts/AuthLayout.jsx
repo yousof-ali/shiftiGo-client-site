bad0079796c4ed307efa72d63866b02d0f33bf21
yousof-ali <yousofali293@gmail.com>
2025-12-08 01:24:57 +0600

    react form

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -5,20 +5,20 @@ import { Link, Outlet } from "react-router";
 const AuthLayout = () => {
   return (
     <Container>
-      <div className="min-h-screen flex flex-col justify-center ">
-        <div className="inline-block">
-          <Link to={"/"}>
-            <img className="w-54 cursor-pointer" src="/logo.png" alt="logo" />
-          </Link>
-        </div>
-        <div className="flex  items-center">
+      <div className="inline-block py-4">
+        <Link to={"/"}>
+          <img className="w-44 cursor-pointer" src="/logo.png" alt="logo" />
+        </Link>
+      </div>
+      <div className="min-h-[80vh] flex flex-col justify-center ">
+        <div className="flex  gap-6 items-center">
           {/* form div  */}
-          <div className="flex-1">
+          <div className="flex-1 ">
             <Outlet></Outlet>
           </div>
 
           {/* image div  */}
-          <div className="flex-1">
+          <div className="flex-1 hidden md:flex ">
             <img src="/loginImage.png" alt="" />
           </div>
         </div>
--- src/pages/Auth/Login/Login.jsx
+++ src/pages/Auth/Login/Login.jsx
@@ -1,13 +1,61 @@
-import { Button } from '@/components/ui/button'
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import React from "react";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div>
-      this is login page
-      <Button>fhghg</Button>
-    </div>
-  )
-}
+      <form className="max-w-sm w-full mx-auto"  onSubmit={handleSubmit(onSubmit)}>
+        <h2 className="text-xl sm:text-2xl lg:text-3xl font-semibold mb-8">Welcome Back!</h2>
+          <div className="flex flex-col gap-1">
+            <label className="label">Email</label>
+          <input
+            type="email"
+            className="p-3 rounded-md   border"
+            placeholder="Email"
+            {...register("email", { required: true })}
+          />
+          </div>
 
-export default Login
+          {errors.email?.type === "required" && (
+            <p className="text-xs text-red-600">Email is required!</p>
+          )}
+
+          <div className="flex flex-col gap-1">
+            <label className="label mt-3">Password</label>
+          <input
+            type="password"
+            className=" p-3 rounded-md border"
+            placeholder="Password"
+            {...register("password", { required: true, minLength: 6 })}
+          />
+          {errors.password?.type === "required" && (
+            <p className="text-xs text-red-600">Password is required!!</p>
+          )}
+          {errors.password?.type === "minLength" && (
+            <p className="text-xs text-red-600">
+              Password will be more than 6 character!!
+            </p>
+          )}
+          </div>
+          <div className="mt-2">
+            <a className="link link-hover ">Forgot password?</a>
+          </div>
+        
+        <Button className={"w-full mt-6 py-6"}>Log In</Button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
